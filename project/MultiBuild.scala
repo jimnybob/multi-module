36be73e56396b9a844ad3d0d9f8e9127004a3154
jimnybob <jawcater@gmail.com>
2017-01-03 15:02:34 +0000

    Add more submodules

--- project/MultiBuild.scala
+++ project/MultiBuild.scala
@@ -34,117 +34,22 @@ object MultiBuild extends Build
   }
 
   lazy val commonSettings = Seq(
-    version := "100.0.0-kostas-SNAPSHOT",
+    version := "100.0.0-SNAPSHOT",
     resolvers += "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases"
   ) ++ targetDirs
 
 
   lazy val multi = (project in file(".")).aggregate(
-//    basecampProject,
-//    commonPlayProject,
-//    fc,
     alexaSmarthomeModel,
-//    fcModelProject,
+    smartiiHomeAlexa,
     smartiiIr
-//    metricsProject,
-//    exportRulesApplicatorProject,
-//    fcAdminService,
-//    searchService,
-//    contentService,
-//    ExporterProject,
-//    dmdRemoteProject,
-//    apiAppJsonProject,
-//    DataManagerProject,
-//    batchProject,
-//    batchCommonProject,
-//    pcc
   ).settings(aggregate in update := false).settings(commonSettings)
 
-//  lazy val contentService = project in file ("content-service") dependsOn (
-//    fcModelProject % "test->test;compile->compile",
-//    bnfModelProject % "test->test;compile->compile",
-//    commonElasticSearchProject % "test->test;compile->compile",
-//    commonPlayProject,
-//    batchCommonProject,
-//    exportRulesApplicatorProject
-//    ) settings commonSettings
-//
-//  lazy val searchService = project in file ("search-service") dependsOn (
-//      fcModelProject,
-//      bnfModelProject % "test->test;compile->compile",
-//      commonElasticSearchProject % "test->test;compile->compile"
-//    ) settings commonSettings
-//
-//  lazy val fcAdminService = project in file ("fc-admin-service") dependsOn (
-//    fcModelProject % "test->test;compile->compile",
-//    bnfModelProject,
-//    commonElasticSearchProject % "test->test;compile->compile"
-//    ) settings commonSettings
-//
-//  lazy val fc = project in file("fc") dependsOn (
-//      bnfModelProject % "test->test;compile->compile",
-//      fcModelProject % "test->test;compile->compile",
-//      metricsProject,
-//      exportRulesApplicatorProject,
-//      commonElasticSearchProject % "test->test"
-//    ) settings commonSettings
 
   lazy val alexaSmarthomeModel = project in file("alexa-smarthome-model") settings commonSettings
 
-//  lazy val fcModelProject = project in file("fc-model") dependsOn bnfModelProject settings commonSettings
-//
-//  lazy val exportRulesApplicatorProject = project in file("export-rules-applicator") dependsOn (bnfModelProject % "test->test;compile->compile") settings commonSettings
-//
-//  lazy val metricsProject = project in file("metrics") settings commonSettings
+  lazy val smartiiHomeAlexa = project in file("smartii-home-alexa") dependsOn(alexaSmarthomeModel) settings commonSettings
 
   lazy val smartiiIr = project in file("smartii-ir") settings commonSettings
 
-//  lazy val ExporterProject = project in file("Exporter") dependsOn(
-//    commonElasticSearchProject,
-//    bnfModelProject % "test->test;compile->compile",
-//    fcModelProject,
-//    exportRulesApplicatorProject,
-//    commonPlayProject
-//    ) settings commonSettings
-//
-//  lazy val apiAppJsonProject=project in file("api-app-json") dependsOn (
-//    bnfModelProject % "test->test;compile->compile",
-//    exportRulesApplicatorProject
-//    ) settings commonSettings
-//
-//  lazy val DataManagerProject = project in file("DataManager") dependsOn(
-//    apiAppJsonProject,
-//    commonElasticSearchProject,
-//    bnfModelProject % "test->test;compile->compile",
-//    commonPlayProject
-//    ) settings commonSettings
-//
-//  lazy val dmdRemoteProject = project in file("dmdRemote") dependsOn(
-//    bnfModelProject,
-//    commonElasticSearchProject,
-//    basecampProject,
-//    commonPlayProject
-//    ) settings commonSettings
-//
-//  lazy val basecampProject = project in file("basecamp") settings commonSettings
-//
-//  lazy val commonPlayProject = project in file("common-play") settings commonSettings
-//
-//  lazy val batchProject = project in file("batch") dependsOn(
-//    DataManagerProject,
-//    ExporterProject,
-//    dmdRemoteProject,
-//    batchCommonProject
-//    ) settings commonSettings
-//
-//  lazy val batchCommonProject = project in file("batchCommon") dependsOn(
-//    commonElasticSearchProject % "test->test;compile->compile"
-//    ) settings commonSettings
-//
-//  lazy val pcc = project in file("publishing-control-centre") dependsOn(
-//    bnfModelProject % "test->test;compile->compile",
-//    commonElasticSearchProject,
-//    batchCommonProject,
-//    commonPlayProject
-//    ) settings commonSettings
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -38,3 +38,5 @@ addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.3.3")
 addSbtPlugin("com.github.ddispaltro" % "sbt-reactjs" % "0.5.2")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-aspectj" % "0.10.4")
+
+addSbtPlugin("com.gilt.sbt" % "sbt-aws-lambda" % "0.4.2")
